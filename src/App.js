902f97be5807a7d4a47a010bce5c070bc791245b
Praneetha <chandrapraneeth@Chandras-MacBook-Air.local>
2024-03-29 12:44:53 -0700

    creds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { S3 } from "@aws-sdk/client-s3";
+import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
@@ -7,13 +7,23 @@ function App() {
   // Create state to store file
   const [file, setFile] = useState(null);
   const [inputText, setInputText] = useState('')
-  const [uploadStatus, setUploadStatus] = useState("idle"); // "idle", "uploading", "success", "error"
+  //const [uploadStatus, setUploadStatus] = useState("idle"); // "idle", "uploading", "success", "error"
 
 
   const handleTextInputChange = (event) => {
       setInputText(event.target.value);
       };
     
+// Function to handle file and store it to file state
+const handleFileChange = (e) => {
+  // Uploaded file
+  const file = e.target.files[0];
+  // Changing file state
+  setFile(file);
+};
+
+
+
 
   // Function to upload file to s3
   const uploadFile = async () => {
@@ -25,39 +35,33 @@ function App() {
 
     //gateway end point to access lambda
     
-    const s3 = new S3({
-      // The transformation for params is not implemented.
-      // Refer to UPGRADING.md on aws-sdk-js-v3 for changes needed.
-      // Please create/upvote feature request on aws-sdk-js-codemod for params.
-      params: { Bucket: S3_BUCKET },
-      region: REGION,
-
-      credentials: {
+      // todo:: Remove this hard code
+      const CREDENTIALS = {
         accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-      },
+        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+      }
+
+      // When no region or credentials are provided, the SDK will use the
+      // region and credentials from the local AWS config.
+      
+      const s3Client = new S3Client({
+        region: REGION,
+        credentials: CREDENTIALS
     });
+      //s3Client.config.credentials().then(console.log)
+      // Uploading file to s3
+      const command = new PutObjectCommand({
+          Bucket: S3_BUCKET,
+          Key: file.name,
+          Body: file
+      });
 
-    // Files Parameters
-
-    const params = {
-      Bucket: S3_BUCKET,
-      Key: file.name,
-      Body: file,
-    };
-
-    // Uploading file to s3
-
-    //this  one line works too
-
-    try {
-      const data = await s3.putObject(params);
-      console.log(data); // This will log the response from S3
-      alert("File uploaded successfully.");
-    } catch (err) {
-      console.error(err);
-      alert("Failed to upload file!");
-    }
+      try {
+          const response = await s3Client.send(command);
+          console.log(response);
+      } catch (err) {
+          console.error(err);
+      }
 
     // await s3.putObject(params).promise().then((err, data) => {
     //     console.log(err);
@@ -82,34 +86,28 @@ function App() {
     // });
     
     // insert into dynamo via lambda
-    const API_PATH='https://ckaxti9hn8.execute-api.us-west-1.amazonaws.com'
-    const dynamoDbData = {
-      id: nanoid(),
-      input_text: inputText,
-      input_file_path: `${S3_BUCKET}/${file.name}`,
-    };
+    // const API_PATH='https://ckaxti9hn8.execute-api.us-west-1.amazonaws.com'
+    // const dynamoDbData = {
+    //   id: nanoid(),
+    //   input_text: inputText,
+    //   input_file_path: `${S3_BUCKET}/${file.name}`,
+    // };
 
-    const response = await fetch(API_PATH, {
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json",
-      },
-      body: JSON.stringify(dynamoDbData),
-    });
+    // const response = await fetch(API_PATH, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type":"application/json",
+    //   },
+    //   body: JSON.stringify(dynamoDbData),
+    // });
 
-    if (!response.ok) {
-      throw new Error("Failed to save data to DynamoDB");
-    }
-    alert("File uploaded and data saved successfully!");
+    // if (!response.ok) {
+    //   throw new Error("Failed to save data to DynamoDB");
+    // }
+    // alert("File uploaded and data saved successfully!");
     
   };
-  // Function to handle file and store it to file state
-  const handleFileChange = (e) => {
-    // Uploaded file
-    const file = e.target.files[0];
-    // Changing file state
-    setFile(file);
-  };
+  
   return (
     <div className="App">
       <div>
